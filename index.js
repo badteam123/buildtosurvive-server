1436c06d5d65fe5da07acb04dc22d7474495aafb
Landon Schroering <88072024+DaRealLando123@users.noreply.github.com>
2024-06-30 03:17:53 +0000

    eaeawe

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
 
 var players = [];
 
-io.on('connection', (socket) => {
-  console.log('➕ '+socket.id);
-  socket.emit('connected');
-  players.push({pid:socket.id});
-  socket.on('disconnect', () => {
-    console.log('➖ '+socket.id)
-    for(let i = 0; i < players.length; i++){
-      if(players[i].pid === socket.id){
-        io.emit('removePlayer',socket.id)
-        players.splice(i,1);
+io.on("connection", (socket) => {
+  console.log("➕ " + socket.id);
+  socket.emit("connected");
+  players.push({ pid: socket.id });
+  socket.on("disconnect", () => {
+    console.log("➖ " + socket.id);
+    for (let i = 0; i < players.length; i++) {
+      if (players[i].pid === socket.id) {
+        io.emit("removePlayer", socket.id);
+        players.splice(i, 1);
       }
     }
-  })
-  socket.on('myPlayerData', (data) => {
+  });
+  socket.on("myPlayerData", (data) => {
     let inArray = false;
-    for(let i = 0; i < players.length; i++){
-      if(players[i].pid === socket.id){
-        players[i] = Object.assign(players[i],data);
+    for (let i = 0; i < players.length; i++) {
+      if (players[i].pid === socket.id) {
+        players[i] = Object.assign(players[i], data);
         inArray = true;
         break;
       }
     }
-    if(!inArray){
-      players.push(Object.assign(players[i],data));
+    if (!inArray) {
+      players.push(Object.assign(players[i], data));
     }
-  })
+  });
 });
 
-setInterval(()=>{
-  for(let i = 0; i < players.length; i++){
+setInterval(() => {
+  for (let i = 0; i < players.length; i++) {
     var emittingPlayers = [];
-    for(let j = 0; j < players.length; j++){
-      if(players[i].pid != players[j].pid){
+    for (let j = 0; j < players.length; j++) {
+      if (players[i].pid != players[j].pid && players[i].x != undefined) {
         emittingPlayers.push(players[j]);
       }
     }
-  io.to(players[i].pid).emit('playerData',emittingPlayers);
+    io.to(players[i].pid).emit("playerData", emittingPlayers);
   }
-},100);
+}, 100);
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
