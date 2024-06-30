691a97dd08a565349ae47ea5dbc593cf7707a6ee
Landon Schroering <88072024+DaRealLando123@users.noreply.github.com>
2024-06-30 23:39:29 +0000

    hrlp

--- index.js
+++ index.js
@@ -13,64 +13,37 @@ var players = [];
 
 let serverData = {
   tickDelay: 150,
-  seed: 1
+  seed: 1,
 };
 
 io.on("connection", (socket) => {
   console.log("➕ " + socket.id);
   socket.emit("connected", serverData);
-  players.push({ pid: socket.id });
+  players[socket.id] = {};
+
   socket.on("disconnect", () => {
     console.log("➖ " + socket.id);
-    for (let i = 0; i < players.length; i++) {
-      if (players[i].pid === socket.id) {
-        io.emit("removePlayer", socket.id);
-        players.splice(i, 1);
-      }
-    }
+    io.emit("removePlayer", socket.id);
+    delete players[socket.id];
   });
+
   socket.on("myPlayerData", (data) => {
-    let inArray = false;
-    for (let i = 0; i < players.length; i++) {
-      if (players[i].pid === socket.id) {
-        players[i] = Object.assign(players[i], data);
-        inArray = true;
-        break;
-      }
-    }
-    if (!inArray) {
-      players.push(Object.assign(players[i], data));
-    }
+    players[data.pid] = Object.assign(players[data.pid], data);
   });
 });
 
 setInterval(() => {
-  for (let i = 0; i < players.length; i++) {
+  for (let i in players) {
     var emittingPlayers = [];
-    for (let j = 0; j < players.length; j++) {
-      if (players[i].pid != players[j].pid && players[i].pos != undefined) {
+    for (let j in players) {
+      if (i != j) {
         emittingPlayers.push(players[j]);
       }
     }
-    io.to(players[i].pid).emit("playerData", emittingPlayers);
+    io.to(i).emit("playerData", emittingPlayers);
   }
 }, serverData.tickDelay);
 
 server.listen(3000, () => {
   console.log("listening on *:3000");
 });
-
-
-io.on("command",handleCommand);
-
-function handleCommand(cmd,arg){
-  console.log(cmd);
-  switch(cmd){
-    case "tickDelay":
-      serverData.tickDelay = arg;
-      break;
-    case "seed":
-      serverData.seed = arg;
-      break;
-  }
-}
